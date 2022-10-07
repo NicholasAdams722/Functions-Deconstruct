a50a2c5ca91ae3ee677b6b0c5d04572363c7a21b
Nicholas Adams <njadams.722@gmail.com>
2022-10-07 10:44:42 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+//Kayaking Trip
+
+const chargeBattery = () => {
+    console.log("Charge battery the night before trip")
+};
+const loadCar = () => {
+    console.log("load car with life vest, paddle, bug spray, sunscreen, snacks, and fishing gear")
+};
+const secureKayak = () => {
+    console.log("Securely strap kayak onto roof of car")
+};
+const launchKayak = () => {
+    console.log("Drag kayak down to water and launch")
+};
+const enjoyDay=() => {
+    console.log("Appreciate nature and stuff")
+};
+const returnHome=() => {
+    console.log("Drive home and take a snooze")
+};
